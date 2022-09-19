8a86b8eba152d477641edb8f836958277700b860
ErrorImCodingkeller <ErrorImCodingkeller@DESKTOP-DB10TFK>
2022-09-19 19:15:06 +0200

    Add project files.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        public static void method1(Label label1)
+        {
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        { 
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Thread test = new Thread(new ThreadStart(TestThread));
+            test.Start();
+        }
+        public delegate void UpdateTextCallback(string message);
+
+        private void TestThread()
+        {
+            for (int i = 0; i <= 1000000000; i++)
+            {
+                Thread.Sleep(1000);
+                richTextBox1.Dispatcher.Invoke(
+                    new UpdateTextCallback(this.UpdateText),
+                    new object[] { i.ToString() }
+                );
+            }
+        }
+        private void UpdateText(string message)
+        {
+            richTextBox1.AppendText(message + "\n");
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Thread test = new Thread(new ThreadStart(TestThread));
+            test.Start();
+        }
+
+        private void richTextBox1_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            Thread test = new Thread(new ThreadStart(TestThread));
+            test.Start();
+        }
+    }
+}
+
